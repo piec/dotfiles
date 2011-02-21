0a011afcd9cd2f7a2af090de4631553d9c95ccb5
Pierre Carru <pierre@pierre-build-server.convidia.concordant.co.uk>
2011-02-21 14:52:28 +0000

    sub-bashrc, setup.sh, updated ignores

---
+++ setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -u
+set -e
+set -x
+
+link() {
+	[ -e $1 ] && mv $1 dotfiles_sv/$1
+	ln -s dotfiles/$1
+}
+
+cd ~
+
+[ ! -e dotfiles ]
+git clone --recursive ssh://pierre@serv.thruhere.net/~pierre/git-repos/dotfiles dotfiles
+
+if [ -e dotfiles_sv ]; then
+	[ -d dotfiles_sv ]
+else
+	mkdir dotfiles_sv
+fi
+
+link .vim
+link .vimrc
+
+link .screenrc
+
+link .emacs
+
+link .toprc
+link .htoprc
+
+#export_line="export PATH=\$PATH:~/dotfiles/bin"
+export_line="source ~/dotfiles/sub-bashrc"
+if ! grep "$export_line" .bashrc ; then
+	echo $export_line >> .bashrc
+fi
+
