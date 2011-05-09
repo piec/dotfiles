4e5dca4025ed8dad94552eba250de0ad2e4e75dd
pierre <pierre.carru@gmail.com>
2011-05-09 19:32:13 +0200

    emacs cua-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,5 +1,6 @@
 (add-to-list 'load-path "~/.emacs.d/")
 (server-start)
+(cua-mode)
 
 ;; C-x k to close emacsclient session (instead of C-x #)
 (add-hook 'server-switch-hook
@@ -36,7 +37,7 @@
 
 ;(set-face-font 'default "-*-fixed-*-*-normal-*-13-*-*-*-*-*-*-*")
 ;(set-face-font 'default "-misc-fixed-medium-r-semicondensed--13-120-75-75-c-60-iso8859-9")
-(set-face-font 'default "-misc-fixed-medium-r-normal--13-120-75-75-c-70-iso8859-9")
+;;(set-face-font 'default "-misc-fixed-medium-r-normal--13-120-75-75-c-70-iso8859-9")
 
 ;;(require 'whitespace)
 ;;(require 'tramp)
