1640f2899f9a4b2236645f78486bde552b6894f1
Pierre <pierre.carru@gmail.com>
2011-07-22 09:12:17 +0200

    better clang autocomplete

--- bin/mouse.sh
+++ bin/mouse.sh
@@ -1,4 +1,4 @@
-xinput --set-prop 10 "Device Accel Profile" 0
-xinput --set-prop 10 "Device Accel Constant Deceleration" 2
-xinput --set-prop 10 "Device Accel Profile" -1
+xinput --set-prop 8 "Device Accel Profile" 0
+xinput --set-prop 8 "Device Accel Constant Deceleration" 2
+xinput --set-prop 8 "Device Accel Profile" -1
 
