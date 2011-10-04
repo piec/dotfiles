1b72d152d9e04a8d5a125780a94458a21191d1ac
Pierre <pierre.carru@gmail.com>
2011-10-04 15:06:43 +0100

    emacs: diff command, diff split direction

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -326,6 +326,23 @@
 	(ad-enable-advice 'isearch-repeat 'after 'isearch-no-fail)
 	(ad-activate 'isearch-repeat)))
 
+;; ediff split vert/horiz
+;; (setq ediff-split-window-function (lambda (&optional arg)
+;;                                     (if (> (frame-width) 150)
+;;                                         (split-window-horizontally arg)
+;;                                       (split-window-vertically arg))))
+(setq ediff-split-window-function (if (> (frame-width) 150)
+                                      'split-window-horizontally
+                                    'split-window-vertically))
+
+;; -diff command line option
+(defun command-line-diff (switch)
+  (let ((file1 (pop command-line-args-left))
+        (file2 (pop command-line-args-left)))
+    (ediff file1 file2)))
+
+(add-to-list 'command-switch-alist '("diff" . command-line-diff))
+
 ;; customize variables
 (custom-set-variables
   ;; custom-set-variables was added by Custom.
