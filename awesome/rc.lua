80eb1cd9569bad05ee2751040b6d2d19e136114f
Pierre <pierre.carru@gmail.com>
2011-10-18 22:07:12 +0200

    awesome: border color when focus

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -376,14 +376,14 @@ end)
 
 client.add_signal("focus", 
 	function(c)
-		--c.border_color = beautiful.border_focus
+		c.border_color = beautiful.border_focus
 		--c.border_color = "#ff0000"
 		--awful.titlebar.add(c, { modkey = modkey })
 	end
 	)
 client.add_signal("unfocus",
 	function(c)
-		--c.border_color = beautiful.border_normal
+		c.border_color = beautiful.border_normal
 		--c.border_color = "#00ff00"
 	end
 	)
