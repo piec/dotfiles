472cc2060472d6fd0cc4d805aee86a07d8f4413b
Pierre <pierre.carru@gmail.com>
2012-04-16 17:15:31 +0200

    tmux: default term set to screen-256color, tmux.number.sh fix

--- tmux/.tmux.number.sh
+++ tmux/.tmux.number.sh
@@ -4,7 +4,7 @@ dest="$1"
 [ "x"${dest} != "x" ]
 tmux list-windows -F "#{window_index}" | grep "^${dest}$" 2>&1 > /dev/null
 ret="$?"
-if [ "x"${ret} == "x0" ] ; then
+if [ "x"${ret} = "x0" ] ; then
 	tmux swap-window -t ":${dest}"
 else
 	tmux move-window -t ":${dest}"
