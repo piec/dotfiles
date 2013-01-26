3c08ed1bb7240adb48b74acaa7f20d18686db5dd
Pierre Carru <pcarru@wyplay.com>
2013-01-26 14:56:06 +0100

    xorg: mouse sensitivity rules

--- bin/mouse.sh
+++ bin/mouse.sh
@@ -1,7 +1,18 @@
-set -xe
-[ x"$1" != x ]
-id="$1"
-xinput --set-prop $id "Device Accel Profile" 0
-xinput --set-prop $id "Device Accel Constant Deceleration" ${2-6}
-xinput --set-prop $id "Device Accel Profile" -1
+#!/bin/sh
+set -x
+
+mouse() {
+    xinput --set-prop "$1" "Device Accel Profile" 0
+    xinput --set-prop "$1" "Device Accel Constant Deceleration" "$2"
+    xinput --set-prop "$1" "Device Accel Profile" -1
+}
+
+mouse_by_name() {
+    id=$(xinput list --id-only "$1")
+    [ "$?" = 0 ] && mouse "$id" "$2"
+}
+
+mouse_by_name "Razer Razer DeathAdder" 5
+mouse_by_name "Microsoft Microsoft 5-Button Mouse with IntelliEye(TM)" 1
+mouse_by_name "Logitech USB Optical Mouse" 1
 
