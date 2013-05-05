577c089fead679a4e836385bf8934f3925fb94e3
Pierre <pierre.carru@gmail.com>
2013-05-05 23:19:23 +0200

    focus_minimize.sh: fail if window doesn't exist

--- bin/focus_minimize.sh
+++ bin/focus_minimize.sh
@@ -14,6 +14,7 @@ if [[ "$1" =~ (^[[:digit:]]*$) ]] ; then
 	window_id=$1
 else
 	window_id=`xdotool search --all --desktop 0 --name $1 | head -n 1`
+	[ "$window_id" = "" ] && exit 1
 fi
 
 if [ "a$active" = "a$window_id" ]; then
