0c1114ae478b26a5c10a101b04df79746d618808
Pierre <pierre.carru@gmail.com>
2014-08-01 22:30:36 +0200

    focus_minimize.sh

--- bin/focus_minimize.sh
+++ bin/focus_minimize.sh
@@ -13,7 +13,7 @@ active=`xdotool getactivewindow`
 if [[ "$1" =~ (^[[:digit:]]*$) ]] ; then
 	window_id=$1
 else
-	window_id=`xdotool search --all --desktop 0 --name $1 | head -n 1`
+	window_id=`(xdotool search --all --desktop 0 --name $1 || xdotool search --all --desktop 1 --name $1) | head -n 1`
 	[ "$window_id" = "" ] && exit 1
 fi
 
