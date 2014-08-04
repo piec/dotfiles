c32ccf40c833baa01ef4887ea178f6f5a1e9fe4b
Pierre Carru <pierre.carru@gmail.com>
2014-08-04 10:22:13 +0200

    bin/focus_minimize.sh

--- bin/focus_minimize.sh
+++ bin/focus_minimize.sh
@@ -2,18 +2,27 @@
 #set -x
 set -e
 
+#exec >>~/log.log
+#exec 2>>~/log.log
+
+#(
+	#IFS=,
+	#echo "$0 [$*]"
+#)
+
 err () {
 	echo "$1"
 	exit 1
 }
 
-[ a$1 != a ] || err "\$1 empty"
+[ "$1" != "" ] || err "\$1 empty"
 
 active=`xdotool getactivewindow` 
 if [[ "$1" =~ (^[[:digit:]]*$) ]] ; then
-	window_id=$1
+	window_id="$1"
 else
-	window_id=`(xdotool search --all --desktop 0 --name $1 || xdotool search --all --desktop 1 --name $1) | head -n 1`
+	#window_id=`xdotool search --all --desktop 0 --name "$1" | head -n 1`
+	window_id=`(xdotool search --all --desktop 0 --name "$1" || xdotool search --all --desktop 1 --name "$1") | head -n 1`
 	[ "$window_id" = "" ] && exit 1
 fi
 
