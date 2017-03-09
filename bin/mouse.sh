b68a6d8b44ec085360d6f7d98340035a702f309d
Pierre Carru <pierre.carru@gmail.com>
2017-03-09 20:00:12 +0100

    mouse.sh: use libinput

--- bin/mouse.sh
+++ bin/mouse.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 set -x
 
-mouse() {
-    xinput --set-prop "$1" "Device Accel Profile" 0
-    xinput --set-prop "$1" "Device Accel Constant Deceleration" "$2"
-    xinput --set-prop "$1" "Device Accel Profile" -1
+function mouse {
+    #xinput --set-prop "$1" "Device Accel Profile" 0
+    #xinput --set-prop "$1" "Device Accel Constant Deceleration" "$2"
+    #xinput --set-prop "$1" "Device Accel Profile" -1
+    xinput set-prop "$1" 'libinput Accel Profile Enabled' 0 1
 }
 
-mouse_by_name() {
+function mouse_by_name {
     id=$(xinput list --id-only "$1")
     [ "$?" = 0 ] && mouse "$id" "$2"
 }
 
-mouse_by_name "Razer Razer DeathAdder" 5
-mouse_by_name "Microsoft Microsoft 5-Button Mouse with IntelliEye(TM)" 1
-mouse_by_name "Logitech USB Optical Mouse" 1
+function main {
+    mouse_by_name "Razer Razer DeathAdder" 5
+    mouse_by_name "Microsoft Microsoft 5-Button Mouse with IntelliEye(TM)" 1
+    mouse_by_name "Logitech USB Optical Mouse" 1
+    mouse_by_name "Razer Razer DeathAdder Chroma" 5
+}
 
