53cbbff0ea33b9378dcf469c4215976a53235759
Pierre Carru <pierre.carru@gmail.com>
2017-07-28 23:29:09 +0200

    basic bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+set -ex
+set -o noclobber
+
+if [ "$1" = "backup" ]; then
+    mkdir -p backup
+    backup=".vimrc .vim .tmux.conf .tmux.number.sh .tmux.local .zshenv"
+    for f in $backup; do
+        if [ -e "$f" ]; then
+            mv -v "$f" backup/
+        fi
+    done
+fi
+
+cd
+if ! [ -e dotfiles ]; then
+    git clone --recursive git@github.com:piec/dotfiles dotfiles
+fi
+
+ln -s dotfiles/.vimrc
+ln -s dotfiles/.vim
+
+ln -s dotfiles/tmux/.tmux.conf
+ln -s dotfiles/tmux/.tmux.number.sh
+touch ~/.tmux.local
+
+cat > ~/.zshenv <<EOF
+ZDOTDIR="$HOME/dotfiles/.zsh"
+[ -e "$ZDOTDIR/.zshenv" ] && . "$ZDOTDIR/.zshenv"
+EOF
+
+if [ -e ~/.bash_history ] && ! [ -e ~/.zhistory ] ; then
+    cp -v ~/.bash_history ~/.zhistory
+fi
