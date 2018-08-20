a54e5a2615c61a86554e14a8375245a25473f055
Pierre Carru <pierre.carru@gmail.com>
2018-08-20 23:36:18 +0200

    awesome: keepassx floating

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -83,7 +83,7 @@ awful.layout.layouts = {
     --awful.layout.suit.spiral,
     --awful.layout.suit.spiral.dwindle,
     awful.layout.suit.max,
-    --awful.layout.suit.max.fullscreen,
+    awful.layout.suit.max.fullscreen,
     --awful.layout.suit.magnifier,
     --awful.layout.suit.corner.nw,
     -- awful.layout.suit.corner.ne,
@@ -660,7 +660,8 @@ awful.rules.rules = {
 local floating = {
   "Pavucontrol",
   "Skype",
-  "keepassx",
+  "Keepass",
+  "Keepassx",
   "Mplayer",
   "pinentry",
   "gimp",
