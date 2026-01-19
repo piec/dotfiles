1bbb43aa4e791c87d771dc5857629d7ba3757e47
Pierre Carru <pierre@carru.fr>
2026-01-19 16:20:23 +0100

    various

--- bin/nowplaying.sh
+++ bin/nowplaying.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -eu
 set -o pipefail
+#refresh="xfce4-panel --plugin-event=genmon-16:refresh:bool:true"
+# see also playerctl metadata --follow
+
 status=$(playerctl status 2>/dev/null)
 
 if [ "$status" = "Playing" ]; then
